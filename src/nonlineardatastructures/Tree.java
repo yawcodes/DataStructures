736c7182decf84666f5dfbf02dddf4506062c653
yawcodes <yawoseiagyemang80@gmail.com>
2023-04-26 23:19:49 -0400

    Adding TreeNode print feature

--- src/nonlineardatastructures/Tree.java
+++ src/nonlineardatastructures/Tree.java
@@ -7,9 +7,28 @@ public class Tree {
         this.root = root;
     }
 
+    public void depthFirstTraversal(TreeNode current) {
+        System.out.print(current.data);
+
+        for (TreeNode child : current.children) {
+            depthFirstTraversal(child);
+        }
+    }
+
     public void printlist(TreeNode root) {
         for (TreeNode node : root.children) {
-            System.out.println();
+            print(node, 0);
+        }
+    }
+
+    public void print(TreeNode current, int level) {
+        String levelMarks = "";
+        for (int i = 0; i < level; i++) {
+            levelMarks += "-- ";
+        }
+        System.out.println(levelMarks + current.data);
+        for (TreeNode child : current.children) {
+            print(child, level + 1);
         }
     }
 }
